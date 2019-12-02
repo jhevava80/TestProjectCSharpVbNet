b9370e4fd34c55b72159c48de69412d5a1364c03
jhon-vargas-sf <jhon.vargas@salesforce.com>
2019-12-02 15:26:08 -0500

    Commit including select datable with Vb net
     select datable with Vb net

--- SolutionTestCVbNet/ClassLibraryVbNet/TestClass.vb
+++ SolutionTestCVbNet/ClassLibraryVbNet/TestClass.vb
@@ -1,6 +1,15 @@
-﻿Public Class TestClass
+﻿Imports System.Data
+
+Public Class TestClass
     Sub prueba(ByRef cad As String)
         cad = "se da garra"
     End Sub
+
+    Function BuscaPregunta(question As String, dt As DataTable) As DataRow
+        Dim row As DataRow
+        row = dt.Select("Pregunta = '" + question + "' ").FirstOrDefault
+
+        Return row
+    End Function
 
 End Class
--- SolutionTestCVbNet/ConsoleAppCore/Program.cs
+++ SolutionTestCVbNet/ConsoleAppCore/Program.cs
@@ -1,5 +1,8 @@
 ﻿using ClassLibraryVbNet;
 using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
 
 namespace ConsoleAppCore
 {
@@ -7,10 +10,56 @@ namespace ConsoleAppCore
     {
         static void Main(string[] args)
         {
+            BuscaPregunta();    
+        }
+
+        private static void BuscaPregunta()
+        {
             TestClass tc = new TestClass();
-            string cad = "";
-            tc.prueba(ref cad);
-            Console.WriteLine(cad);
+            DataTable dtExcel = ExcelToDataTable("C:\\TestData\\BDQuestionProcuraduria.xlsx");
+            string pregunta = "¿ Cual es la Capital de Colombia (sin tilde)?";  //"Esta es la pregunta";
+            var row = tc.BuscaPregunta(pregunta, dtExcel);
+            if (row != null)
+            {
+                Console.WriteLine(String.Format(" Pregunta: {0} respuesta: {1} ", row[0], row[1]));
+            }
+            else
+            {
+                Console.WriteLine(String.Format("Question: {0} was not found", pregunta));
+            }
+        }
+
+        public static DataTable ExcelToDataTable(string filePath)
+        {
+            DataTable dtexcel = new DataTable();
+            bool hasHeaders = false;
+            string HDR = hasHeaders ? "Yes" : "No";
+            string strConn;
+            if (filePath.Substring(filePath.LastIndexOf('.')).ToLower() == ".xlsx")
+                strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=\"Excel 12.0;HDR=" + HDR + ";IMEX=0\"";
+            else
+                strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=\"Excel 8.0;HDR=" + HDR + ";IMEX=0\"";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            //Looping Total Sheet of Xl File
+            /*foreach (DataRow schemaRow in schemaTable.Rows)
+            {
+            }*/
+            //Looping a first Sheet of Xl File
+            DataRow schemaRow = schemaTable.Rows[0];
+            string sheet = schemaRow["TABLE_NAME"].ToString();
+            if (!sheet.EndsWith("_"))
+            {
+                string query = "SELECT  * FROM [" + sheet + "]";
+                OleDbDataAdapter daexcel = new OleDbDataAdapter(query, conn);
+                dtexcel.Locale = CultureInfo.CurrentCulture;
+                daexcel.Fill(dtexcel);
+            }
+
+            conn.Close();
+            return dtexcel;
+
         }
     }
 }
