e49de6cd9b3d76295dd837849b87de3257a6801b
jhonv80 <jhevava80@gmail.com>
2019-12-05 21:39:36 -0500

    commit 5-12-2019
    commit 5-12-2019

--- SolutionTestCVbNet/ClassLibraryVbNet/TestClass.vb
+++ SolutionTestCVbNet/ClassLibraryVbNet/TestClass.vb
@@ -7,8 +7,17 @@ Public Class TestClass
 
     Function BuscaPregunta(question As String, dt As DataTable) As DataRow
         Dim row As DataRow
-        row = dt.Select("Pregunta = '" + question + "' ").FirstOrDefault
+        Dim existe As Boolean
+        Dim respuesta As String
 
+        row = dt.Select("Pregunta = '" + question + "' ").FirstOrDefault
+        If row Is Nothing Then
+            existe = False
+            respuesta = ""
+        Else
+            existe = True
+            respuesta = row("Respuesta")
+        End If
         Return row
     End Function
 
